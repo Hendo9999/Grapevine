69d05119b62c3c416ad37ef35b2ad893d2ba9c85
Henderson <hen232@csiro.au>
2019-10-14 17:44:51 +1030

    loading data files

--- Scripts/Grapevine_analyses.R
+++ Scripts/Grapevine_analyses.R
@@ -10,89 +10,76 @@ library(readxl)#for reading Excel files
 #file with harvester data
 raw_harvester <- read_xlsx("Data/0192 Mature laminae chloride summary with harvester info.xlsx")
 
-#files with chloride data for each replicate & batch
+#files with chloride data for each replicate & batch 
+#import rows from specific sheets with chloride data for samples only (not standards)
 
-#Biological replicate #1, sample batches #1 - #6
-raw_r1b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 9)
-raw_r1b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 10)
-raw_r1b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 12)
-raw_r1b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 14)
-raw_r1b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 18)
-raw_r1b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 20)
+#Biological replicate #1, sample batches #1 - #6, rows with pertinent data
+raw_r1b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 9, n_max = 45)
+raw_r1b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 10, n_max = 44)
+raw_r1b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 12, n_max = 43)
+raw_r1b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 14, n_max = 41)
+raw_r1b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 18, n_max = 48)
+raw_r1b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 20, n_max = 30)
 
 #Biological replicate #2, sample batches #1 - #6
-raw_r2b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 8)
-raw_r2b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 9)
-raw_r2b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 11)
-raw_r2b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 13)
-raw_r2b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 17)
-raw_r2b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 19)
+raw_r2b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 8, cell_rows(73:115))
+raw_r2b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 9, cell_rows(71:114))
+raw_r2b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 11, n_max = 47)
+raw_r2b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 13, cell_rows(29:69))
+raw_r2b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 17, n_max = 47)
+raw_r2b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 19, n_max = 53)
 
 #Biological replicate #3, sample batches #1 - #6
-raw_r3b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 8)
-raw_r3b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 9)
-raw_r3b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 11)
-raw_r3b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 13)
-raw_r3b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 17)
-raw_r3b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 19)#note: excel file tab is labelled Cl-R2B6 (not Cl-R3B6). Check with Deidre Blackmore if this is a typo.  
-
-#filter the raw files to select for rows with chloride data only (not standard data)
-C_r1b1_fil <- raw_r1b1 %>%
-  drop_na(Code) %>% #Chloride data has values in Code column, standards do not so can be removed on this basis
-  filter("Code" != "Code")
+raw_r3b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 8, n_max = 44)
+raw_r3b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 9, n_max = 43)
+raw_r3b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 11, n_max = 44)
+raw_r3b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 13, n_max = 48)
+raw_r3b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 17, n_max = 45)
 
 
-C_r1b2_fil <- raw_r1b2 %>%
-  drop_na(Code)
+#Files with Standards data for each replicate & batch
 
-C_r1b3_fil <- raw_r1b3 %>%
-  drop_na(Code)
+#17.1mM Cl standards data for each sample batch
+raw_r1b1_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 9, range = "F49:J67")
+raw_r1b2_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 10, range = "F48:J66")
+raw_r1b3_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 12, range = "B48:E66")
+raw_r1b4_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 14, range = "B49:E67")
+raw_r1b5_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 18, range = "B54:E72")
+raw_r1b6_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 20, range = "B38:E56")
 
-C_r1b4_fil <- raw_r1b4 %>%
-  drop_na(Code)
+raw_r2b1_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 8, range = "G122:J140")
+raw_r2b2_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 9, range = "G118:J136")
+raw_r2b3_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 11, range = "D51:G69")
+raw_r2b4_17mM_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 13, range = "D74:G92")
+raw_r2b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 17, n_max = 47)
+raw_r2b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 19, n_max = 53)
 
-C_r1b5_fil <- raw_r1b5 %>%
-  drop_na(Code)
+raw_r3b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 8, n_max = 44)
+raw_r3b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 9, n_max = 43)
+raw_r3b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 11, n_max = 44)
+raw_r3b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 13, n_max = 48)
+raw_r3b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 17, n_max = 45)
 
-C_r1b6_fil <- raw_r1b6 %>%
-  drop_na(Code)
+view(raw_r1b1_grape_st)
+#Bulk "grapevine" standards data for each sample batch
+raw_r1b1_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 9, range = "M49:O57")
+raw_r1b2_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 10, range = "M48:O54")
+raw_r1b3_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 12, range = "H48:J61")
+raw_r1b4_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 14, range = "H49:J58")
+raw_r1b5_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 18, range = "H54:J65")
+raw_r1b6_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP1.xlsx", sheet = 20, range = "H38:J49")
 
-C_r2b1_fil <- raw_r2b1 %>%
-  drop_na(Code) 
+#Biological replicate #2, sample batches #1 - #6
+raw_r2b1_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 8, range = "M122:O131")
+raw_r2b2_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 9, range = "M118:O128")
+raw_r2b3_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 11, range = "J51:L61")
+raw_r2b4_grape_st <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 13, range = "J74:L85")
+raw_r2b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 17, n_max = 47)
+raw_r2b6 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP2.xlsx", sheet = 19, n_max = 53)
 
-C_r2b2_fil <- raw_r2b2 %>%
-  drop_na(Code)
-
-C_r2b3_fil <- raw_r2b3 %>%
-  drop_na(Code)
-
-C_r2b4_fil <- raw_r2b4 %>%
-  drop_na(Code)
-
-C_r2b5_fil <- raw_r2b5 %>%
-  drop_na(Code)
-
-C_r2b6_fil <- raw_r2b6 %>%
-  drop_na(Code)
-
-C_r3b1_fil <- raw_r3b1 %>%
-  drop_na(Code) 
-
-C_r3b2_fil <- raw_r3b2 %>%
-  drop_na(Code)
-
-C_r3b3_fil <- raw_r3b3 %>%
-  drop_na(Code)
-
-C_r3b4_fil <- raw_r3b4 %>%
-  drop_na(Code)
-
-C_r3b5_fil <- raw_r3b5 %>%
-  drop_na(Code)
-
-C_r3b6_fil <- raw_r3b6 %>%
-  drop_na(Code) %>% 
-  Filter(C_r3b6_fil, "Code" != Code)
-
-
-view(C_r3b6_fil)
+#Biological replicate #3, sample batches #1 - #6
+raw_r3b1 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 8, n_max = 44)
+raw_r3b2 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 9, n_max = 43)
+raw_r3b3 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 11, n_max = 44)
+raw_r3b4 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 13, n_max = 48)
+raw_r3b5 <- read_xlsx("Data/3&4. 0192 barcodes (LAM)-BIOMASS.REP3.xlsx", sheet = 17, n_max = 45)
